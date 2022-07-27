8839d42ce7f3681d2d9d6841d6e7b85acb397251
redMulla <ivassillycharite@gmail.com>
2022-07-27 11:54:47 +0300

    banner background changed

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const toggleBtn = document.getElementsByClassName('toggle-btn')[0]
 const navbar = document.getElementsByClassName('navbar')[0]
 const accountNav = document.getElementsByClassName('account')[0]
-const header = document.querySelectorAll('header')[0]
-const slideImg = document.getElementById('slideImg')
+const header = document.querySelector('header')
+const banner = document.querySelector('.home-banner')
 
 toggleBtn.addEventListener('click', () => {
     header.classList.toggle('active')
@@ -11,22 +11,38 @@ toggleBtn.addEventListener('click', () => {
     toggleBtn.classList.toggle('active')
 })
 
-const images = new Array (
-    "ressource/images/cow1.jpg",
-    "ressource/images/cow2.jpg",
-    "ressource/images/milk3.jpg",
-    "ressource/images/milk1.jpg",
-)
+const bannerOption = {
+    rootMargin: "-640px 0px 0px 0px"
+};
 
-const length = images.length;
-let i = 0;
+const bannerObserver = new IntersectionObserver((entries, bannerObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            header.classList.add('nav-scrolled')
+        } else {
+            header.classList.remove('nav-scrolled')
+        }
+    })
+}, bannerOption);
 
-const slider = () => {
-    if(i > length - 1) {
-        i = 0
-    }
+bannerObserver.observe(banner);
 
-    slideImg.src = images[i];
-    i++;
-    setTimeout('slider()', 3000)
-}
+// const images = new Array (
+//     "ressource/images/cow1.jpg",
+//     "ressource/images/cow2.jpg",
+//     "ressource/images/milk3.jpg",
+//     "ressource/images/milk1.jpg",
+// )
+
+// const length = images.length;
+// let i = 0;
+
+// const slider = () => {
+//     if(i > length - 1) {
+//         i = 0
+//     }
+
+//     slideImg.src = images[i];
+//     i++;
+//     setTimeout('slider()', 3000)
+// }
